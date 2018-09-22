f4bf81fe12c8ac0f749cd48b5b07f1ae07cbb111
Zhi Shen Yong <zyong@wustl.edu>
2018-09-22 17:13:32 -0500

    adding some comments and making the code look nice

--- matchery_client/src/screens/Login.js
+++ matchery_client/src/screens/Login.js
@@ -1,26 +1,53 @@
+// IMPORT COMPONENTS
 import React, { Component } from 'react';
+
+// IMPORT STYLING
 import './Login.css';
 
+// COMPONENT CLASS
 class Login extends React.Component {
+
+  // Component constructor
   constructor(props) {
     super(props);
-    this.state = {
+    this.state = { // Initialize login values
       username: "",
       password: "",
     }
   }
+
+  // This function updates either the username
+  // or the password state when the respective
+  // input fields are updated
   handleChange = ({target}) => {
     this.setState({[target.name]: target.value});
   }
+
+  // This function calls a parent function
+  // in the Main component to handle the
+  // login process
   childHandleLogin = (e) => {
     this.props.parentHandleLogin(e);
   }
+
+  // This function calls a parent function
+  // in the Main component to handle the
+  // signup process
   childHandleSignup = (e) => {
-    this.props.parentHandleSignup(e, this.state.username, this.state.password);
+    this.props.parentHandleSignup(
+      e,
+      this.state.username,
+      this.state.password
+    );
   }
+
+  // Render the component
   render() {
+
+    // Return the component frame
     return (
       <div className="loginBox">
+      
         <form>
           <div className="centerRow">
             <input
@@ -43,14 +70,23 @@ class Login extends React.Component {
             />
           </div>
           <div className="centerRow">
-            <button className="loginButton" onClick={(e) => {this.childHandleLogin(e)}}>Login</button>
+            <button
+              className="loginButton"
+              onClick={(e) => {this.childHandleLogin(e)}}>
+              Login
+            </button>
           </div>
           <div className="centerRow">
-            <button className="signUpButton" onClick={(e) => {this.childHandleSignup(e)}}>Sign Up</button>
+            <button
+              className="signUpButton"
+              onClick={(e) => {this.childHandleSignup(e)}}>
+              Sign Up
+            </button>
           </div>
         </form>
+
       </div>
-    )
+    );
   }
 }
 export default Login;
--- matchery_client/src/screens/Main.js
+++ matchery_client/src/screens/Main.js
@@ -8,10 +8,10 @@ import './Main.css'; // Header and background styling
 // Note: Component-specific styling is imported through
 // the components themselves
 
-// MAIN APP COMPONENT
+// MAIN APP COMPONENT CLASS
 class App extends Component {
 
-  // App constructor
+  // Component constructor
   constructor(props) {
     super(props);
     this.state = { // Initialize screen states
@@ -27,20 +27,22 @@ class App extends Component {
   }
 
   // This function is triggered by a child
-  // function in the Login component
+  // function in the Login component and
+  // handles the login process
   parentHandleLogin = (e) => {
     e.preventDefault();
     this.setState({showLogin: false, showCandidate: true});
   }
 
   // This function is triggered by a child
-  // function in the Login component
+  // function in the Login component and
+  // handles the signup process
   parentHandleSignup = (e, username, password) => {
     e.preventDefault();
     alert("Sign up attempted with: " + username + " " + password);
   }
 
-  // Render the components of the Main application
+  // Render the Main application
   render() {
 
     // Styling constants for showing different screens
@@ -49,22 +51,24 @@ class App extends Component {
 
     // Return the app frame (header and background)
     return (
-      <div className="header">
-        <div className="row">
-          <button className="headerButton">Learn More</button>
+      <div>
+
+        <div className="header">
+            <button className="headerButton">Learn More</button>
+            <h1 className="headerTitle">Matchery</h1>
         </div>
-        <div className="row">
-          <h1 className="headerTitle">Matchery</h1>
+
+        <div style={showLogin}>
+          <Login
+            parentHandleLogin={this.parentHandleLogin}
+            parentHandleSignup={this.parentHandleSignup}
+          />
         </div>
-      </div>
-      <div style={showLogin}>
-        <Login
-          parentHandleLogin={this.parentHandleLogin}
-          parentHandleSignup={this.parentHandleSignup}
-        />
-      </div>
-      <div style={showCandidate}>
-        <Candidate />
+
+        <div style={showCandidate}>
+          <Candidate />
+        </div>
+
       </div>
     );
   }
